5bdf971f5d9188bd5220636f0fd1d6064616bfdd
Adam Thomas <thomasadam83@hotmail.com>
2022-04-18 10:16:13 -0400

    Added .gitignore

--- app.js
+++ app.js
@@ -1,1 +1,38 @@
-console.log("hello!");
+
+const button = document.querySelector('#contact-form button');
+const form = document.querySelector('#contact-form form');
+
+const formDiv = document.getElementById('contact-form');
+const thankyouDiv = document.getElementById('thankyou-message');
+
+thankyouDiv.style.display = 'none';
+
+button.addEventListener('click', function(e){
+
+    e.preventDefault();
+
+    const XHR = new XMLHttpRequest();
+
+    // Bind the FormData object and the form element
+    const FD = new FormData( form );
+
+    // Define what happens on successful data submission
+    XHR.addEventListener( "load", function(event) {
+
+      formDiv.style.display = 'none';
+      thankyouDiv.style.display = 'block';
+
+    } );
+
+    // Define what happens in case of error
+    XHR.addEventListener( "error", function( event ) {
+      alert( 'Oops! Something went wrong.' );
+    } );
+
+    // Set up our request
+    XHR.open( "POST", "send.php" );
+
+    // The data sent is what the user provided in the form
+    XHR.send( FD );
+
+});
--- send.php
+++ send.php
@@ -1,1 +1,31 @@
-{"status":"complete"}
+<?php
+
+
+// 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
+
+// Uncomment next line if you're not using a dependency loader (such as Composer)
+// require_once '<PATH TO>/sendgrid-php.php';
+
+use SendGrid\Mail\Mail;
+
+$email = new Mail();
+$email->setFrom("test@example.com", "Example User");
+$email->setSubject("Sending with Twilio SendGrid is Fun");
+$email->addTo("test@example.com", "Example User");
+$email->addContent("text/plain", "and easy to do anywhere, even with PHP");
+$email->addContent(
+    "text/html", "<strong>and easy to do anywhere, even with PHP</strong>"
+);
+$sendgrid = new \SendGrid(getenv('SENDGRID_API_KEY'));
+try {
+    $response = $sendgrid->send($email);
+    print $response->statusCode() . "\n";
+    print_r($response->headers());
+    print $response->body() . "\n";
+} catch (Exception $e) {
+    echo 'Caught exception: '.  $e->getMessage(). "\n";
+}
+
+print_r($_POST);
+
+die('{"status":"complete"}');
